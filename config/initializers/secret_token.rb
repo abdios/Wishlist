4cdafa4dbb0f64a49efa58455f9e99cc1eb32cc1
DoubleCaster <doublecaster@NidavellirRails.Yggdrasil>
2013-10-22 20:41:23 +0300

    Rails initial commit with command - rails new wishlist -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishlist::Application.config.secret_key_base = '7ea88445a7c294cae5fcaa9269d891b0d707909e54afd40bd3d0d25f968c5ec9f954fa6c551423c74348c3dd0928971f785b1211a35ae09e247b640dc973d4c8'
