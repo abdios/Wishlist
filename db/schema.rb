70c49bde39861e09a9b3009793c27f0bce17f2db
DoubleCaster <doublecaster@NidavellirRails.Yggdrasil>
2013-10-22 20:45:24 +0300

    Used rails default generator resource with options username email password_digest admin:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022174152) do
+ActiveRecord::Schema.define(version: 20131022174414) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
