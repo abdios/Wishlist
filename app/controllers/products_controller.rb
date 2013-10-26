552f5eb9c2b5857bfe252b19cd8b293373c4af93
DoubleCaster <doublecaster@NidavellirRails.Yggdrasil>
2013-10-26 22:28:16 +0300

    Added to users controller and created nav bar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,41 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.create(params[:product].permit(:name, :description, :price))
+		if @product.save
+			redirect_to root_url
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		if @product.update_attributes(params[:product].permit :name, :description, :price)
+			redirect_to root_url
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to root_url
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(params[:user].permit :username, :email, :password, :password_confirmation)
+		@user.admin = false
+		if @user.save
+			redirect_to users_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user].permit :username, :email, :password, :password_confirmation)
+			redirect_to users_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_path
+	end
 end
